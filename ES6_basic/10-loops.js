633945f088cda1f26ef74a0708a652dd302e870f
kraoshin <killian.kerleau@outlook.fr>
2025-03-24 10:35:01 +0100

    add new exercises

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const arrayNew = [];
+    for (const elem of array) {
+      arrayNew.push(appendString + elem);
+    }
+  
+    return arrayNew;
+  }
