d526ac815e284aab13a01ebf4d946eefffaf38e3
kraoshin <killian.kerleau@outlook.fr>
2025-04-26 11:47:31 +0200

    add all exercises

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Task 1 """
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Returns a list of delays in ascending order """
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
